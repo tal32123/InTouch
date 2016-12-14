2475fbecadec7df65c2e6dc83c07d801c79b661c
tal <tal32123@gmail.com>
2016-12-14 04:03:55 -0600

    Add detail screen

---
+++ InTouch/app/src/main/java/tk/talcharnes/intouch/ContactDetailActivity.java
@@ -0,0 +1,13 @@
+package tk.talcharnes.intouch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_detail);
+    }
+}
--- InTouch/app/src/main/java/tk/talcharnes/intouch/MainActivity.java
+++ InTouch/app/src/main/java/tk/talcharnes/intouch/MainActivity.java
@@ -1,13 +1,13 @@
 package tk.talcharnes.intouch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), ContactDetailActivity.class);
+                startActivity(intent);
             }
         });
+
+
     }
 
     @Override
