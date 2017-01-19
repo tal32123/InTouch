24f78fe1588025415e9f2b503e810f6b4fc27ed2
tal <tal32123@gmail.com>
2017-01-19 16:21:20 -0600

    Fix bug notifications are cancelled on device restart

---
+++ InTouch/app/src/main/java/tk/talcharnes/intouch/RestartNotificationService.java
@@ -0,0 +1,63 @@
+//package tk.talcharnes.intouch;
+//
+//import android.app.PendingIntent;
+//import android.app.Service;
+//import android.content.Intent;
+//import android.database.Cursor;
+//import android.os.IBinder;
+//
+//import tk.talcharnes.intouch.data.ContactsContract;
+//
+//import static tk.talcharnes.intouch.Utility.ACTION_CALL_NOTIFICATION;
+//
+//public class RestartNotificationService extends Service {
+//    public RestartNotificationService() {
+//        final String ACTION_CALL_NOTIFICATION = "action_call";
+//        final String ACTION_SEND_TEXT = "action_send_text";
+//        final String ACTION_NOTIFICATION = "action_notification";
+//    }
+//
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        Cursor cursor = getApplicationContext().getContentResolver().query(ContactsContract.ContactsEntry.CONTENT_URI, null, null, null, null);
+//
+//        int contact_idIndex = cursor.getColumnIndex(ContactsContract.ContactsEntry._ID);
+//        int nameIndex = cursor.getColumnIndex(ContactsContract.ContactsEntry.COLUMN_NAME);
+//        int photoThumbnailUriIndex = cursor.getColumnIndex(tk.talcharnes.intouch.data.ContactsContract.ContactsEntry.COLUMN_PHOTO_THUMBNAIL_URI);
+//        int phoneNumberIndex = cursor.getColumnIndex(tk.talcharnes.intouch.data.ContactsContract.ContactsEntry.COLUMN_PHONE_NUMBER);
+//        int messageListIndex = cursor.getColumnIndex(tk.talcharnes.intouch.data.ContactsContract.ContactsEntry.COLUMN_MESSAGE_LIST);
+//        int textFrequencyIndex = cursor.getColumnIndex(ContactsContract.ContactsEntry.COLUMN_TEXT_FREQUENCY);
+//        int callFrequencyIndex = cursor.getColumnIndex(ContactsContract.ContactsEntry.COLUMN_CALL_FREQUENCY);
+//        int callCounterIndex = cursor.getColumnIndex(ContactsContract.ContactsEntry.COLUMN_CALL_NOTIFICATION_COUNTER);
+//        int textCounterIndex = cursor.getColumnIndex(ContactsContract.ContactsEntry.COLUMN_TEXT_NOTIFICATION_COUNTER);
+//        int notificationTimeIndex = cursor.getColumnIndex(ContactsContract.ContactsEntry.COLUMN_NOTIFICATION_TIME);
+//
+//
+//        String name = cursor.getString(nameIndex);
+//        String photoThumbnailUri = cursor.getString(photoThumbnailUriIndex);
+//        String phoneNumber = cursor.getString(phoneNumberIndex);
+//        String messageList = cursor.getString(messageListIndex);
+//        int textFrequency = cursor.getInt(textFrequencyIndex);
+//        int callFrequency = cursor.getInt(callFrequencyIndex);
+//        int contact_id = cursor.getInt(contact_idIndex);
+//        int callCounter = cursor.getInt(callCounterIndex);
+//        int textCounter = cursor.getInt(textCounterIndex);
+//        long notificationTime = cursor.getLong(notificationTimeIndex);
+//
+//        if (cursor != null && cursor.moveToFirst()) {
+//            while (cursor.moveToNext()) {
+//
+//                PendingIntent callIntent = Utility.createNotificationPendingIntent(name, phoneNumber, messageList, Integer.toString(contact_id),
+//                        photoThumbnailUri, ACTION_CALL_NOTIFICATION, getApplicationContext());
+//
+//                Utility.createNotifications(callIntent, getApplicationContext(), notificationTime, callFrequency);
+//
+//                PendingIntent textIntent = Utility.createNotificationPendingIntent(name, phoneNumber, messageList, Integer.toString(contact_id),
+//                        photoThumbnailUri, ACTION_CALL_NOTIFICATION, getApplicationContext());
+//
+//                Utility.createNotifications(textIntent, getApplicationContext(), notificationTime, textFrequency);
+//            }
+//        }
+//        return null;
+//    }
+//}
